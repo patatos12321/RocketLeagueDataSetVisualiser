1b5e79a31b7008810079d1527f26f5a0f49b192f
patatos12321 <31021380+patatos12321@users.noreply.github.com>
2018-03-31 16:43:09 -0400

    initial commit

---
+++ RocketLeagueDataSetVisualiser/Assets/Frame.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Frame
+{
+    public int BallX;
+    public int BallY;
+    public int BallVelocityX;
+    public int BallVelocityY;
+    public int PlayerX;
+    public int PlayerY;
+    public int EnemyPlayerX;
+    public int EnemyPlayerY;
+    public int Label;
+
+    public static List<Frame> GetListFrameFromDataSet(string dataSetFile)
+    {
+        //File format
+        //BallX,BallY,BallVelocityX,BallVelocityY,PlayerX,PlayerY,EnemyPlayerX,EnemyPlayerY,Label
+        Debug.Log("File.ReadAllLines(" + dataSetFile);
+        string[] dataSetLines = File.ReadAllLines(dataSetFile);
+        List<Frame> frames = new List<Frame>();
+
+        foreach (string dataSetLine in dataSetLines)
+        {
+            Debug.Log(dataSetLine);
+            string[] arrayDataSetLine = dataSetLine.Split(',');
+            try
+            {
+                Frame currentFrame = new Frame()
+                {
+                    BallX = int.Parse(arrayDataSetLine[0]),
+                    BallY = int.Parse(arrayDataSetLine[1]),
+                    BallVelocityX = int.Parse(arrayDataSetLine[2]),
+                    BallVelocityY = int.Parse(arrayDataSetLine[3]),
+                    PlayerX = int.Parse(arrayDataSetLine[4]),
+                    PlayerY = int.Parse(arrayDataSetLine[5]),
+                    EnemyPlayerX = int.Parse(arrayDataSetLine[6]),
+                    EnemyPlayerY = int.Parse(arrayDataSetLine[7]),
+                    Label = int.Parse(arrayDataSetLine[8]),
+                };
+
+                frames.Add(currentFrame);
+            }
+            catch
+            {
+
+            }
+        }
+        return frames;
+    }
+}
---
+++ RocketLeagueDataSetVisualiser/Assets/ReplayVisualizer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplayVisualizer : MonoBehaviour {
+
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Ball;
+    public string ReplayFile;
+
+    List<Frame> frames = new List<Frame>();
+    int frameNumber = 0;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("ReplayFile");
+        frames = Frame.GetListFrameFromDataSet(ReplayFile);
+    }
+    
+	
+	// Update is called once per frame
+	void Update () {
+        Frame frame = frames[frameNumber];
+
+        //I have ot switch the Y and the X
+        Vector3 positionPlayer1 = new Vector3(frame.PlayerY, frame.PlayerX, 0);
+        Player1.transform.position = positionPlayer1;
+
+        Vector3 positionPlayer2 = new Vector3(frame.EnemyPlayerY, frame.EnemyPlayerX, 0);
+        Player2.transform.position = positionPlayer2;
+
+        Vector3 positionBall = new Vector3(frame.BallY, frame.BallX, 0);
+        Ball.transform.position = positionBall;
+        
+        if (frameNumber < frames.Count)
+        {
+            frameNumber++;
+        }
+    }
+}
